6aad3575f197ae6bfb3ada5e1f7bb308ae6a9cf5
Native Porter <port@native.com>
2026-04-24 16:53:17 +0530

    Fix: Rename shadowing variables i, m, r2, r3 to avoid class collision

---
+++ app/src/main/java/javax/microedition/lcdui/Font.java
@@ -0,0 +1,11 @@
+package javax.microedition.lcdui;
+
+public class Font {
+    public static final int STYLE_PLAIN = 0;
+    public static final int SIZE_SMALL = 0;
+    public static final int FACE_SYSTEM = 0;
+    
+    public static Font getFont(int face, int style, int size) {
+        return new Font();
+    }
+}
--- app/src/main/java/javax/microedition/rms/RecordStore.java
+++ app/src/main/java/javax/microedition/rms/RecordStore.java
@@ -5,6 +5,8 @@ public class RecordStore {
         return new RecordStore();
     }
     public void closeRecordStore() throws RecordStoreException {}
-    public byte[] getRecord(int id) throws RecordStoreException { return null; }
+    public byte[] getRecord(int id) throws RecordStoreException { return new byte[0]; }
     public int addRecord(byte[] data, int offset, int numBytes) throws RecordStoreException { return 0; }
+    public int getNumRecords() throws RecordStoreException { return 0; }
+    public void setRecord(int id, byte[] newData, int offset, int numBytes) throws RecordStoreException {}
 }
